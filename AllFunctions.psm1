b89b33a9a6388ecffcf042d94d27bcf1815c2663
Master <mohammadbashir.employee@gmail.com>
2025-10-13 04:30:40 +0300

    Add files via upload

--- AllFunctions.psm1
+++ AllFunctions.psm1
@@ -2775,7 +2775,9 @@ function Ins-AcrobatPro {
 	Write-Host -f C "`r`n *** Installing Adobe Acrobat Pro DC *** `r`n"
 	Disable-DefenderRealtimeProtection
 	# Acrobat 2025
-	$URL = "https://drive.google.com/file/d/173Ck-8lXon9LnjhQb8viROIcWY-YHcXn/view"
+	$URL = "https://drive.google.com/file/d/12JLKxAPZEadisRBhw7iXR9Kx2qfdFO1h/view"
+	# Acrobat 2024
+	$URL = "https://drive.google.com/file/d/1pHMRUoBq2_wSsfXQaOKWGaVcbnA4drrm/view"
 	$Key = "AIzaSyBjpiLnU2lhQG4uBq0jJDogcj0pOIR9TQ8"
 	$DDURL = Convert-GoogleDriveUrl -URL $URL -Key $Key
 	if ($DDURL) { Write-Host "Downloading..."; Start-BitsTransfer -Source $DDURL -Destination "$env:TEMP\AdobeAcrobatProDCx64.exe"  -EA SilentlyContinue | Out-Null }
@@ -6498,7 +6500,7 @@ function Get-TargetType {
 	# 2. If it's a fully qualified path
 	if (Test-Path $Target -PathType Leaf -ErrorAction SilentlyContinue) {
 		$ext = [System.IO.Path]::GetExtension($Target)
-		if ($ext -match '^\.(exe|com|bat|cmd|ps1)$') {
+		if ($ext -match '^\.(exe|com|bat|cmd|ps1|msi|msp)$') {
 			return "Executable"
 		}
 		return "File"
